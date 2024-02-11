8dc8c0dfd09371125bffb8a20e6ddebe2e5f36f9
Hy Nguyen <giahy316@gmail.com>
2024-02-11 15:43:31 -0700

    fixed typo

--- app/components/Listings.tsx
+++ app/components/Listings.tsx
@@ -4,7 +4,7 @@ import { list } from "postcss";
 export default async function Listings() {
   var listings = [];
   listings = await getListings();
-  if (listing) {
+  if (listings) {
     listings = listings.map((item: any) => {
       const newItem = [...item]; // create copy of item array
       newItem[0] = newItem[0].toString(); // convert first element (Date object) to string
