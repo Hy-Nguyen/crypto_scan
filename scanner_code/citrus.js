0fbd1ee2d92ae1cd6c9ed6bd311342f7f83e7b4c
Hy Nguyen <giahy316@gmail.com>
2024-02-12 13:40:41 -0700

    Changed from .gs to .js

--- scanner_code/citrus.js
+++ scanner_code/citrus.js
@@ -1,6 +1,9 @@
-function getCitrus(address = "") {
+const fetch = require("node-fetch");
+
+async function getCitrus(address = "") {
   var wallet =
     "428JqXgFg3yjuMoa4ZkKi7MBJLn2thvpSTH6HS2NLQC1";
+
   // Citrus
   var citrusUrl = `https://citrus.famousfoxes.com/citrus/userSocials/${wallet}`;
   var citrusHeader = {
@@ -10,25 +13,38 @@ function getCitrus(address = "") {
       "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
     },
-    muteHttpExceptions: true,
   };
 
-  var citrusResponse = UrlFetchApp.fetch(
-    citrusUrl,
-    citrusHeader
-  );
+  try {
+    var citrusResponse = await fetch(
+      citrusUrl,
+      citrusHeader
+    );
 
-  var citrusres = JSON.parse(citrusResponse);
-  var currentLoaned =
-    citrusres["loanSummary"]["currentLoaned"];
-  var pendingOffers =
-    citrusres["loanSummary"]["pendingOffers"];
+    if (!citrusResponse.ok) {
+      throw new Error(
+        `HTTP error! status: ${citrusResponse.status}`
+      );
+    } else {
+      var citrusres = await citrusResponse.json();
+      var currentLoaned =
+        citrusres["loanSummary"]["currentLoaned"];
+      var pendingOffers =
+        citrusres["loanSummary"]["pendingOffers"];
+      var citrusTotal =
+        currentLoaned + pendingOffers;
 
-  var citrusTotal = currentLoaned + pendingOffers;
-
-  // Console Log Tests for Citrus:
-  console.log(citrusres);
-  console.log(
-    "Citrus Loans Total: " + citrusTotal
-  );
+      // Console Log Tests for Citrus:
+      console.log(citrusres);
+      console.log(
+        "Citrus Loans Total: " + citrusTotal
+      );
+    }
+  } catch (e) {
+    console.log(
+      "There was a problem with your fetch operation: " +
+        e.message
+    );
+  }
 }
+getCitrus();
