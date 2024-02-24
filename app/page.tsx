c5dcb57d0255c39615d9e625b16e71ae7ac33a28
Miguel Uriostegui <muriostegui00@gmail.com>
2024-02-24 02:51:20 -0700

    Added wallet counter to dipsplay the number of wallets listed, next to "Your Wallet" header

--- app/page.tsx
+++ app/page.tsx
@@ -147,40 +147,31 @@ export default function Home() {
             <Card shadow="lg" className="w-1/2">
               <CardHeader className="flex gap-0 justify-center bg-p3-alt-light">
                 <Image
-                  alt="nextui logo"
-                  height={200}
-                  radius="none"
-                  src="https://solana.com/_next/static/media/logotype.e4df684f.svg"
-                  width={400}
-                />
-              </CardHeader>
-              <Divider />
-              <CardHeader className="text-lg font-bold flex justify-center">
-                <h2 className="text-center">
-                  Your Wallets
-                </h2>
-              </CardHeader>
-              <Divider />
+                alt="nextui logo"
+                height={200}
+                radius="none"
+                src="https://solana.com/_next/static/media/logotype.e4df684f.svg"
+                width={400}/>
+                </CardHeader>
 
-              <CardBody className=" p-0 min-h-52">
-                <div>
-                  {walletHolding.map(
-                    (
-                      address: string,
-                      i: number
-                    ) => (
-                      <>
-                        <WalletAddress
-                          address={address}
-                          key={i}
-                          refresh={deleteRefresh}
-                        />
-                      </>
-                    )
-                  )}
-                </div>
-              </CardBody>
-            </Card>
+                <Divider />
+                <CardHeader className="text-lg font-bold flex justify-center">
+                  <h2 className="text-center">
+                     Your Wallets ({walletHolding.length})
+                  </h2>
+                  </CardHeader>
+                  <Divider />
+                  <CardBody className=" p-0 min-h-52">
+                    <div>
+                      {walletHolding.map((address: string, i: number) => (
+                      <WalletAddress
+                      address={address}
+                      key={i}
+                      refresh={deleteRefresh}
+                      /> ))}
+                      </div>
+                    </CardBody>
+              </Card>
           </div>
         </div>
       </main>
